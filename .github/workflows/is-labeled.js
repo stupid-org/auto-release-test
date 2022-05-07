a3b9e71efa3328c77a9e0be8cc637aee2b116289
James Sumners <james@sumners.email>
2022-05-07 11:41:56 -0400

    Add is labeled workflow

---
+++ .github/workflows/is-labeled.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = async function isLabeled({ github, context, core }) {
+  const pr = await github.rest.pulls.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.issue.number,
+  });
+
+  const targetLabels = ['semver-major', 'semver-minor', 'semver-patch', 'skip-release']
+  const foundTargetLabels = []
+  for (const label of pr.labels) {
+    if (targetLabels.includes(label.name) === false) {
+      continue
+    }
+    foundTargetLabels.push(label.name)
+  }
+
+  switch (foundTargetLabels.length) {
+    case 0: {
+      core.setFailed('No required labels found.')
+      break
+    }
+
+    case 1: {
+      break
+    }
+
+    default: {
+      core.setFailed('Too many required labels found: ' + foundTargetLabels.join())
+    }
+  }
+}
